1fbac705ff4575da4307c79aa15763e15fabee14
farmanwebdev <saqibfarman633@gmail.com>
2026-02-20 11:51:38 +0500

    Prepare project for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/GYFTech/', // 👈 MUST match your GitHub repo name exactly, case-sensitive
 })
